3e0b005c8af960d1af621cdd9cc27ebc1c13ee36
Ivan Santos <ivansantos.ronem@gmail.com>
2024-06-25 09:17:38 -0300

    init: construção do ambiente

---
+++ src/main/java/ics/luizalabs/desafio/xy_inc/PointOfInterestXyIncApplication.java
@@ -0,0 +1,13 @@
+package ics.luizalabs.desafio.xy_inc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointOfInterestXyIncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointOfInterestXyIncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ics/luizalabs/desafio/xy_inc/PointOfInterestXyIncApplicationTests.java
@@ -0,0 +1,13 @@
+package ics.luizalabs.desafio.xy_inc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointOfInterestXyIncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
