785b90183a40df65d306a6b6445f5a29ac03002f
Ivan Santos <ivansantos.ronem@gmail.com>
2024-06-26 11:30:02 -0300

    feat: implementação de  validação ao controller

--- src/main/java/ics/luizalabs/desafio/xy_inc/dto/PontoDeInteresseDTO.java
+++ src/main/java/ics/luizalabs/desafio/xy_inc/dto/PontoDeInteresseDTO.java
@@ -1,8 +1,9 @@
 package ics.luizalabs.desafio.xy_inc.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -11,9 +12,13 @@ import java.time.LocalDateTime;
 @Builder
 public class PontoDeInteresseDTO {
 
+    @NotEmpty(message = "CAMPO LOCAL NÃO PODE SER VAZIO E/OU NULO")
+    @NotBlank(message = "CAMPO LOCAL NÃO PODE SER VAZIO E/OU NULO")
     private String localPoi;
 
+    @NotNull(message = "COORDENADA X NÃO PODE SER VAZIO E/OU NULO")
     private Double coordX;
 
+    @NotNull(message = "COORDENADA Y NÃO PODE SER VAZIO E/OU NULO")
     private Double coordY;
 }
--- src/main/java/ics/luizalabs/desafio/xy_inc/exceptions/PontoDeInteresseExceptionHandler.java
+++ src/main/java/ics/luizalabs/desafio/xy_inc/exceptions/PontoDeInteresseExceptionHandler.java
@@ -2,6 +2,7 @@ package ics.luizalabs.desafio.xy_inc.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -24,7 +25,7 @@ public class PontoDeInteresseExceptionHandler {
     }
 
     @ExceptionHandler(value = {RegraDeNegocioException.class})
-    public ResponseEntity<ExceptionMessage> handlerExceptionBadRequest(RuntimeException ex) {
+    public ResponseEntity<ExceptionMessage> handlerNegativeException(RuntimeException ex) {
 
         ExceptionMessage message = new ExceptionMessage(
                 HttpStatus.BAD_REQUEST.value(),
@@ -36,7 +37,19 @@ public class PontoDeInteresseExceptionHandler {
     }
 
     @ExceptionHandler(value = {MethodArgumentTypeMismatchException.class})
-    public ResponseEntity<ExceptionMessage> handlerExceptionFormatNumber(MethodArgumentTypeMismatchException ex) {
+    public ResponseEntity<ExceptionMessage> handlerFormatNumberException(MethodArgumentTypeMismatchException ex) {
+
+        ExceptionMessage message = new ExceptionMessage(
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST,
+                new Date(),
+                ex.getMessage());
+
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResponseEntity<ExceptionMessage> handlerArgumentNotValid(MethodArgumentNotValidException ex) {
 
         ExceptionMessage message = new ExceptionMessage(
                 HttpStatus.BAD_REQUEST.value(),
